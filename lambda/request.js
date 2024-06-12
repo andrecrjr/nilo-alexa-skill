4bf866123f160a24ccc96dbae3f13c1670c167c6
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-06-12 20:37:13 -0300

    fix: user logged

--- lambda/request.js
+++ lambda/request.js
@@ -3,7 +3,7 @@ const axios = require("axios")
 const apiBase = axios.create({ baseURL: "https://2e54-179-70-50-192.ngrok-free.app" })
 
 const getUserAuth = async (accessToken) => {
-    const data = await apiBase.get("/users/profile", {
+    const { data } = await apiBase.get("/users/profile", {
         headers: {
             "Authorization": `Bearer ${accessToken}`
         }
